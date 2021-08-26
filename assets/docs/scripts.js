5d1b0ebe96a721769c7182f7a65ca44648415d97
Austen Myers <austenc.id@pm.me>
2021-08-26 17:22:16 -0600

    BOOZLED

--- assets/docs/scripts.js
+++ assets/docs/scripts.js
@@ -1,27 +1,34 @@
 // javascript challenge 04 
-const huskers = [{id: 'left'},{id: "right"}]
-var opponent = []
+const huskers = [{id: 'left'},{id: "right"},{id: 'l-boozle'},{id: "r-boozle"}]
+var opponentL = []
+var opponentR = []
 const L_HUSKER_ELEM = document.getElementById('left')
 const R_HUSKER_ELEM = document.getElementById('right')
-// function locateHuskerById (id){
-    
-// }
+const L_BOOZLE_ELEM = document.getElementById('l-boozle')
+const R_BOOZLE_ELEM = document.getElementById('r-boozle')
+var leftBoozlePoints = 0
+var rightBoozlePoints = 0
+startBoozles()
 function locateHuskerById(id){
-    let found = huskers.find(husker => husker.id === id);
-    console.log(found)
+    let foundHusker = huskers.find(husker => husker.id === id);
+    console.log(foundHusker)
+}
+function startBoozles(huskers){
+    L_BOOZLE_ELEM.innerText= `${leftBoozlePoints}` + '% boozled'
+    R_BOOZLE_ELEM.innerText= `${rightBoozlePoints}` + '% boozled'
 }
 
 function awooo(huskerId){
     locateHuskerById(huskerId);
     if (huskerId === 'left'){
-        const husker = locateHuskerById('right')
-        opponent.push(huskerId)
-        R_HUSKER_ELEM.textContent = 'Boozled 1/10'
-        console.log(opponent)
+        const rightboozle = locateHuskerById('r-boozle')
+        rightBoozlePoints += 10
+        R_BOOZLE_ELEM.innerText = `${rightBoozlePoints}` + '% boozled' 
+        console.log(rightBoozlePoints)
     }
     if (huskerId === 'right'){
-        const husker = locateHuskerById('left')
-        L_HUSKER_ELEM.textContent = 'Boozled 1/10'
-        console.log(opponent)
+        const leftboozle = locateHuskerById('l-boozle')
+        leftBoozlePoints += 10
+        L_BOOZLE_ELEM.innerText = `${leftBoozlePoints}` + '% boozled' 
     }
 }
