51fb0c714ea4f0355201101fb86a66e83a97f6a5
Austen Myers <austenc.id@pm.me>
2021-08-27 14:19:04 -0600

    basic frame

---
+++ assets/scripts/load.js
@@ -0,0 +1,29 @@
+//TITLE checkpoint 2 boozled
+//SECTION data
+const buttons = [
+    {id: 0, name: 'main'},
+    {id: 1, name: 'upgrade-1'},
+    {id: 2, name: 'upgrade-2'},
+    {id: 3, name: 'upgrade-3'},
+    {id: 4, name: 'upgrade-4'}
+]
+
+var boozled = [100] //NOTE consequence\goal
+var awooos = [0] //NOTE resource
+var hyper = [25] //NOTE multiplier
+
+startBoozles()
+
+//SECTION draw functions
+function startBoozles(){
+    //REFERENCE L_BOOZLE_ELEM.innerText= `${leftBoozlePoints}` + '% boozled'
+    // R_BOOZLE_ELEM.innerText= `${rightBoozlePoints}` + '% boozled'
+}
+
+//SECTION click functions
+
+//SECTION data functions
+//REFERENCE function locateHuskerById(id){
+//     let foundHusker = huskers.find(husker => husker.id === id);
+//     console.log(foundHusker)
+
